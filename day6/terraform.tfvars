d19cbd37e2d0098ad54bf36a817764bca5aa4d39
someshwarreddy8 <somudasari8@gmail.com>
2025-06-28 14:11:36 +0530

    day6 practice

---
+++ day6/terraform.tfvars
@@ -0,0 +1,2 @@
+ec2_ami = "ami-0d03cb826412c6b0f"
+ec2_aws_instance_type = "t2.micro"
---
+++ day6/variables.tf
@@ -0,0 +1,11 @@
+variable "ec2_ami" {
+  description = "This is ami"
+  default = ""
+  type = string
+}
+
+variable "ec2_aws_instance_type" {
+  description = "This is aws_instance"
+  type = string
+  default = ""
+}
