9580554caef57b7cd6df264bd6ba70d2ae236580
someshwarreddy8 <somudasari8@gmail.com>
2025-06-29 19:38:37 +0530

    fix

--- day7/main.tf
+++ day7/main.tf
@@ -17,21 +17,23 @@ resource "aws_subnet" "sub1" {
 
 }
 
-  resource "aws_subnet" "sub2" {
+resource "aws_subnet" "sub2" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "10.0.0.64/26"
-  depends_on = [ aws_vpc.myvpc ]
+  depends_on = [aws_vpc.myvpc]
 
   tags = {
     Name = "sub2"
+  }
+}
 
 resource "aws_subnet" "sub3" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "10.0.0.128/26"
-  depends_on = [ aws_vpc.myvpc ]
+  depends_on = [aws_vpc.myvpc]
 
   tags = {
-     Name = "sub3"
+    Name = "sub3"
   }
 
 }
