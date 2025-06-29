6c8f019d630fda4d1c5c0b3cc28587c0d9a1e020
root <root@ip-172-31-40-228.ap-south-1.compute.internal>
2025-06-29 14:07:35 +0000

    added subnet4

--- day7/main.tf
+++ day7/main.tf
@@ -14,15 +14,24 @@ resource "aws_subnet" "sub1" {
   tags = {
     Name = "sub1"
   }
+
 }
 
-resource "aws_subnet" "sub2" {
+  resource "aws_subnet" "sub2" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "10.0.0.64/26"
   depends_on = [ aws_vpc.myvpc ]
 
   tags = {
     Name = "sub2"
+
+resource "aws_subnet" "sub3" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.128/26"
+  depends_on = [ aws_vpc.myvpc ]
+
+  tags = {
+     Name = "sub3"
   }
 
 }
