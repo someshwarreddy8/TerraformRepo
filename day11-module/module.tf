73065c65147127d75a8c4ea9c0fddaca6df0052e
someshwarreddy8 <somudasari8@gmail.com>
2025-07-08 08:56:44 +0530

    module changes with repo link

--- day11-module/module.tf
+++ day11-module/module.tf
@@ -1,5 +1,6 @@
 module "myownmodule" {
-  source = "../day11"
+  source = "github.com/someshwarreddy8/TerraformRepo/day11"
+#   source = "../day11"
   ami_id = "ami-0d03cb826412c6b0f"
   instance_type = "t2.micro"
   bucketName = "Testbucket08072025"
